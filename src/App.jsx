c9a4cec7a5c201bb74b21079f284001288b64e86
foxamanda2 <fox.amanda2@gmail.com>
2021-02-24 19:07:38 -0500

    Currency Converter working and created a pull down menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [currency, setCurrencyConversion] = useState({ rates: {} })
+
+  const [amount, setAmount] = useState(0)
+
+  useEffect(async () => {
+    const resp = await axios.get('https://api.ratesapi.io/api/latest?base=USD')
+    setCurrencyConversion(resp.data)
+  }, [])
+
+  useEffect(() => {
+    console.log(currency)
+  }, [currency])
+
+  let optionItems = Object.entries(currency.rates).map(
+    ([currencyCode, currencyDetails]) => {
+      return (
+        <option>
+          {' '}
+          {currencyCode}:{(currencyDetails * amount).toFixed(2)}
+        </option>
+      )
+    }
+  )
+
+  return (
+    <div>
+      <h1>Unit Conversion</h1>
+      <h2>
+        {' '}
+        USD:
+        <input
+          type="number"
+          placeholder="Enter value"
+          value={amount}
+          onChange={function (event) {
+            setAmount(event.target.value)
+          }}
+        />
+      </h2>
+      <h2>Conversion Options:</h2>
+      <select>{optionItems}</select>
+    </div>
+  )
 }
